6706679b5077437724250129108e0e0a4ebc972a
Lin Liu <lin.liu@citrix.com>
2019-01-12 15:53:24 +0800

    runnable
    Signed-off-by: Lin Liu <lin.liu@citrix.com>

--- flasky.py
+++ flasky.py
@@ -1,10 +1,13 @@
 import os
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 from app.models import User, Role
+from flask_script import  Manager,Shell
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+manager = Manager(app)
 
 
 @app.shell_context_processor
@@ -18,3 +21,8 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("db", MigrateCommand)
+if __name__ == "__main__":
+    manager.run()
